dcc10cf17bb5a245abc2cc7df000a5f7ef753a34
ChantiqueNababan <chantiquenababan03@gmail.com>
2024-11-23 09:28:39 +0700

    menambahkan virus.js dan css

--- src/pages/Virus.js
+++ src/pages/Virus.js
@@ -1,99 +1,75 @@
 import React from "react";
-import '../styles/Virus.css';
+import { Link } from "react-router-dom";
+import './Virus.css';
 
-export const Virus = () => {
+function Virus() {
   return (
-    <div className="wrapper">
-    <div className="card">
-      <h1> Virus</h1>
-      <p>Pelajari Yuk tentang Virus</p>
-    </div>
+    <div className="card-wrapper">
+      <div className="text-section">
+        <h1 className="text-title">Selamat Datang di Dunia Virus</h1>
+      </div>
 
-    <div className="text-box"><br></br>
-        <h2>Sejarah Virus</h2>
-        <img src="./images/Virus0.jpg"></img>
-        <p>Istilah virus berasal dari bahasa Latin,virion yang artinya racun. Sejarah penemuan virus dimulai pada tahun 1883 dengan ditemukannya penyakit yang menyebabkan adanya bintik-bintik kuning pada daun tembakau. Penyakit tersebut kemudian dikenal dengan istilah penyakit mosaik tembakau. Beberapa ilmuwan yang terlibat dalam penemuan virus adalah sebagai berikut :</p>
-        <p>a. Adolf Meyer. </p>
-        <p>Pada tahun 1883, Adolf Meyer, seorang ilmuwan Jerman mengamati penyakit yang menyebabkan adanya bintik-bintik kuning pada daun tembakau. Meyer kemudian melakukan percobaan dengan menyemprotkan getah yang diekstraksi dari tanaman tembakau yang sakit ke tanaman tembakau yang sehat. Ternyata, tanaman tembakau yang sehat menjadi sakit. Meyer kemudian mencoba mengamati daun tembakau yang sakit dengan menggunakan mikroskop biasa. Akan tetapi, ia tidak dapat menemukan bakteri yang diduga menjadi penyebab penyakit tersebut. Meyer kemudian menyimpulkan bahwa bakteri penyebab penyakit pada tanaman tembakau berukuran lebih kecil dari bakteri biasanya.</p>
-        <p>b. Dmitri Ivanovsky. </p>
-        <p>Pada tahun 1892, Dmitri Ivanovsky, seorang ilmuwan Rusia melakukan percobaan dengan menyaring getah tanaman tembakau yang sakit dengan menggunakan saringan bakteri. Selanjutnya, hasil saringan tersebut ditularkan pada tanaman tembakau yang sehat. Ternyata, tanaman tembakau yang sehat tersebut menjadi sakit. Ivanovsky kemudian menyimpulkan bahwa penyebab penyakit pada tanaman tembakau adalah bakteri patogenik yang sangat kecil atau bakteri penghasil toksin yang dapat melewati saringan. </p>
-        <p>c. Martinus Beijerinck. </p>
-        <p>Pada tahun 1897, Martinus Beijerinck, seorang ilmuwan Belanda melakukan percobaan untuk membuktikan bahwa agen penyebab penyakit pada tanaman tembakau dapat berkembang biak. Beijerinck menyemprotkan getah tanaman yang sudah disaring ke tanaman yang sehat. Setelah tanaman yang sehat menjadi sakit, getah tanaman tersebut digunakan untuk menginfeksi tanaman berikutnya, dan seterusnya hingga beberapa kali pemindahan. Ternyata, melalui beberapa kali pemindahan, sifat patogennya tidak berkurang. Agen tersebut juga berbeda dengan bakteri, karena tidak dapat dikembangbiakkan di dalam cawan petri yang berisi nutrisi. Selain itu, juga tidak dapat dinonaktifkan menggunakan alkohol. Beijerinck kemudian menyimpulkan bahwa agen tersebut adalah partikel yang lebih kecil danlebih sederhana dari bakteri. Beijerinck kemudian menyebutnya sebagai virus lolos saring (filterable virus). </p>
-        <p>d. Wendell Meredith Stanley. </p>
-        <p>Pada tahun 1935, Wendell Meredith Stanley, seorang ilmuwan Amerika berhasil mengkristalkan partikel penyebab penyakit pada tanaman tembakau. Penyakit ini kemudian dikenal dengan nama Tobacco Mosaic Virus (TMV).</p>
-    </div><br></br>
+      <div className="des">
+      <p className="text-description">
+          Virus adalah salah satu makhluk mikroskopis paling menarik di dunia. Mereka bukan hanya ancaman, 
+          tapi juga kunci untuk memahami biologi dan inovasi teknologi. Temukan cerita di balik virus di sini!
+        </p></div>
 
-    <div className="text-box">
-      <h2>Ciri-ciri Virus</h2>
-      <h5>Berdasarkan gambar di atas, ada ciri-ciri yang dimiliki virus yaitu :</h5>
-        <p>a.Virus berukuran sangat kecil, berkisar 0,02-0,3 µm (1 µm = 1/1.000 mm), dan paling besar berukuran 200 µm, karena itu virus hanya dapat dilihat dengan mikroskop elektron. </p>
-        <p>b.Tubuh virus terdiri atas selubung proton (kapsid), dan bahan inti. Bahan inti berupa RNA (Ribonucleic acid) dan DNA (Deoxiribonucleic acid). </p>
-        <p>c.Virus tidak mempunyai membran dan organel-organel sel yang penting bagi kehidupan. </p>
-        <p>d.Virus hanya dapat bereproduksi jika berada dalam sel hidup atau jaringan hidup. </p>
-        <p>e.Biasanya stabil pada pH 5.0 sampai 9.0. </p>
-        <p>f.Virus dapat dikristalkan seperti benda mati. Bentuk virus bermacam-macam ada yang berbentuk batang, bola, atau bulat, berbetuk peluru, dan berbentuk</p>
-        <p>g.Aktivitas virus dapat dihilangkan oleh sinar ultra ungu dan sinar X tetapi zat antibiotik dan zat antibakteri lain tidak berpengaruh terhadapnya.</p>
-    </div><br></br>
+        <div className="del1">
+        <h2 className="isi1">Asal Usul Virus</h2>
+        <h6>Istilah virus berasal dari bahasa Latin,virion yang artinya racun. Sejarah penemuan virus dimulai pada tahun 1883 dengan 
+          ditemukannya penyakit yang menyebabkan adanya bintik-bintik kuning pada daun tembakau. 
+          Penyakit tersebut kemudian dikenal dengan istilah penyakit mosaik tembakau. Beberapa ilmuwan yang terlibat dalam penemuan virus adalah sebagai berikut :</h6>
+        <p><strong>a. Adolf Meyer.</strong>  Pada tahun 1883, Adolf Meyer, seorang ilmuwan Jerman mengamati penyakit yang menyebabkan adanya bintik-bintik kuning pada daun tembakau. Meyer kemudian melakukan percobaan dengan menyemprotkan getah yang diekstraksi dari tanaman tembakau yang sakit ke tanaman tembakau yang sehat. 
+        Ternyata, tanaman tembakau yang sehat menjadi sakit. Meyer kemudian mencoba mengamati daun tembakau yang sakit dengan menggunakan mikroskop biasa. Akan tetapi, ia tidak dapat menemukan bakteri yang diduga menjadi penyebab penyakit tersebut. Meyer kemudian menyimpulkan bahwa bakteri penyebab penyakit pada tanaman tembakau berukuran lebih kecil dari bakteri biasanya. </p>
+        <p><strong>b. Dmitri Ivanovsky.</strong>  Pada tahun 1892, Dmitri Ivanovsky, seorang ilmuwan Rusia melakukan percobaan dengan menyaring getah tanaman tembakau yang sakit dengan menggunakan saringan bakteri. 
+        Selanjutnya, hasil saringan tersebut ditularkan pada tanaman tembakau yang sehat. Ternyata, tanaman tembakau yang sehat tersebut menjadi sakit. Ivanovsky kemudian menyimpulkan bahwa penyebab penyakit pada tanaman tembakau adalah bakteri patogenik yang sangat kecil atau bakteri penghasil toksin yang dapat melewati saringan. </p>
+        <p><strong>c. Martinus Beijerinck.</strong> Pada tahun 1897, Martinus Beijerinck, seorang ilmuwan Belanda melakukan percobaan untuk membuktikan bahwa agen penyebab penyakit pada tanaman tembakau dapat berkembang biak. 
+        Beijerinck menyemprotkan getah tanaman yang sudah disaring ke tanaman yang sehat. Setelah tanaman yang sehat menjadi sakit, getah tanaman tersebut digunakan untuk menginfeksi tanaman berikutnya, dan seterusnya hingga beberapa kali pemindahan. Ternyata, melalui beberapa kali pemindahan, sifat patogennya tidak berkurang. 
+        Agen tersebut juga berbeda dengan bakteri, karena tidak dapat dikembangbiakkan di dalam cawan petri yang berisi nutrisi. Selain itu, juga tidak dapat dinonaktifkan menggunakan alkohol. 
+        Beijerinck kemudian menyimpulkan bahwa agen tersebut adalah partikel yang lebih kecil danlebih sederhana dari bakteri. Beijerinck kemudian menyebutnya sebagai virus lolos saring (filterable virus). </p>
+        <p><strong>d. Wendell Meredith Stanley.</strong> Pada tahun 1935, Wendell Meredith Stanley, seorang ilmuwan Amerika berhasil mengkristalkan partikel penyebab penyakit pada tanaman tembakau. Penyakit ini kemudian dikenal dengan nama Tobacco Mosaic Virus (TMV).</p>
+      </div>  
 
-    <div className="text-one">
-      <h2>Struktur Virus</h2>
-      <h5>Virus tidak termasuk sel (aseluler), karena tidak memiliki bagian-bagian sel seperti dinding sel, membran sel, sitoplasma, inti sel, dan organel-organel lainnya. Partikel virus yang lengkap disebut virion. Secara umum, struktur virus diwakili oleh bakteriofag yang berbentuk seperti huruf T.</h5>
-      <img src="./images/Virus1.jpeg"></img>
-      <h6>Struktur dari bakteriofag sebagai berikut :</h6>
-      <p>a. Kepala </p>
-      <p>Kepala merupakan bagian dalam kepala virus berisi asam nukleat, sedangkan bagian luarnya diselubungi oleh kapsid. Kepala virus bakteriofag berbentuk polihedral dengan jenis asam nukleat DNA</p>
-      <p>b. Leher</p>
-      <p>Leher merupakan bagian yang menghubungkan kepala dan ekor. Leher juga menjadi saluran keluarnya asam nukleat menuju ekor.</p>
-      <p>c. Ekor</p>
-      <p>Ekor merupakan bagian yang berfungsi untuk menempel pada sel inang. Ekor terdiri atas serabut ekor dan lempeng dasar. Serabut ekor berfungsi melekatkan diri pada sel inang. Sementara itu, lempeng dasar yang berisi jarum penusuk berfungsi untuk menginjeksikan DNA ke dalam sel inang.</p>
-    </div>
+      <h2>Yuk ketahui lebih lanjut !</h2>
 
-    <div className="text-one1">
-      <h2>Bentuk-bentuk Virus</h2>
-      <h5>Virus memiliki bentuk yang bermacam-macam, seperti batang, bulat, oval (peluru), filamen (benang), polihedral, dan seperti huruf T.</h5>
-      <img src="./images/Virus2.jpg"></img>
-      <p>a.Bentuk batang, misalnya TMV (Tobacco Mosaic Virus). </p>
-      <p>b.Bentuk batang dengan ujung oval seperti peluru, misalnya Rhabdovirus </p>
-      <p>c.Bentuk bulat, misalnya HIV (Human Immunodeficiency Virus) dan Orthomyxovirus</p>
-      <p>d.Bentuk filamen (benang), misalnya virus Ebola. </p>
-      <p>e.Bentuk polihedral, misalnya Adenovirus. </p>
-      <p>f.Bentuk seperti huruf T, misalnya bakteriofag, yaitu virus yang menyerang bakteri Escherichia coli.</p>
-    </div>
+      <div className="card-container">
+        <img src="./images/Virus1.jpeg" alt="Virus" className="card-img" />
+        <h1 className="card-title">Sejarah Virus</h1>
+        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
+        <Link to="/virus-details" className="card-btn">Learn More</Link>
+      </div>
 
-    <div className="text-one1">
-      <h2>Replikasi Virus</h2>
-      <h5>Proses perkembangbiakan virus ada dua macam, yaitu daur litik dan daur lisogenik.</h5>
-      <img src="./images/Virus3.jpg"></img>
-      <h6>1. Daur Litik </h6>
-      <p>Daur litik terjadi jika pertahanan sel inang lebih lemah dibandingkan dengan daya infeksi virus. Virus yang mampu bereproduksi dengan daur litik disebut virus virulen. Pada daur litik, sel inang akan pecah dan mati, serta akan terbentuk virion-virion baru. Seluruh tahapan dalam daur litik berlangsung dengan cepat. Tahapan-tahapan tersebut adalah adsorpsi, penetrasi, sintesis dan replikasi, pematangan (perakitan), serta lisis. </p>
-      <p>a. Adsorpsi </p>
-      <p>Virion menempel pada reseptor spesifik sel inang dengan menggunakan bagian serabut ekornya. Molekul reseptor ini berbeda-beda untuk setiap jenis virus, ada yang berupa protein dan ada yang berupa oligosakarida. Ada tidaknya reseptor juga menentukan patogenesis virus, yaitu mekanisme infeksi dan perkembangan penyakit oleh virus. Sebagai contoh, virus polio hanya dapat melekat pada sel saraf pusat dan saluran usus primata, virus HIV hanya berikatan dengan reseptor T CD4 pada sel sistem imun, atau virus rabies yang hanya berinteraksi dengan reseptor asetilkolin. </p>
-      <p>b. Penetrasi </p>
-      <p>Ujung serabut ekor membuat lubang untuk menembus dinding dan membran sel inang. Selanjutnya, virus menginjeksikan materi genetiknya sehingga kapsid virus menjadi kosong (mati).</p>
-      <p>c. Sintesis dan replikasi</p>
-      <p>DNA virus menghidrolisis dan mengendalikan materi genetik sel inang untuk membuat asam nukleat (salinan genom) dan protein komponen virus. Selanjutnya berlangsung tahap replikasi, yaitu pembentukan bagian-bagian tubuh virus yang baru. </p>
-      <p>d. Pematangan atau perakitan </p>
-      <p>Asam nukleat dan protein hasil sintesis dan replikasi dirakit menjadi partikel partikel virus yang lengkap sehingga terbentuk virion-virion baru. </p>
-      <p>e. Lisis </p>
-      <p>Virus menghasilkan enzim lisozim, yaitu enzim yang dapat merusak dinding sel inang. Dinding sel yang rusak mengakibatkan terjadinya osmosis, sehingga sel inang membesar dan akhirnya pecah. Partikel virus yang baru akan keluar dari sel inang dan menyerang sel inang yang lain.</p>
+      <div className="card-container">
+        <img src="./images/Virus2.jpg" alt="Virus" className="card-img" />
+        <h1 className="card-title">Sejarah Virus</h1>
+        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
+        <Link to="/virus-details" className="card-btn">Learn More</Link>
+      </div>
 
-      <h6>2. Daur Lisogenik </h6>
-      <p>Daur lisogenik terjadi jika pertahanan sel inang lebih baik dibandingkan dengan daya infeksi virus. Sel inang pada daur ini tidak segera pecah, bahkan dapat bereproduksi secara normal. Pada daur lisogenik, replikasi genom virus tidak menghancurkan sel inangnya. DNA virus bakteriofag akan berinteraksi dengan kromosom sel inang membentuk profag. Jika sel inang yang mengandung profag membelah diri untuk bereproduksi, profag akan diwariskan kepada sel-sel anakannya. Profag di dalam sel anakan dapat aktif dan keluar dari kromosom sel inang untuk masuk ke dalam tahapan-tahapan daur litik. Virus yang dapat bereproduksi dengan daur litik dan lisogenik disebut virus temperat, misalnya fag λ. </p>
-      <p>Tahapan-tahapan dalam daur lisogenik adalah adsorpsi dan infeksi, penetrasi, penggabungan, pembelahan, serta sintesis. </p>
-      <p>a. Adsorpsi </p>
-      <p>Virion menempel pada reseptor spesifik sel inang dengan menggunakan bagian serabut ekornya. </p>
-      <p>b. Penetrasi </p>
-      <p>Virus menginjeksikan materi genetiknya ke dalam sel inang sehingga kapsid virus menjadi kosong (mati). </p>
-      <p>c. Penggabungan </p>
-      <p>DNA virus bakteriofag bergabung dengan DNA bakteri (sel inang) membentuk profag. Dalam bentuk profag, sebagian besar gen berada dalam fase tidak aktif, tetapi ada sedikitnya satu gen yang selalu aktif. Gen aktif berfungsi mengkode protein reseptor. Protein reseptor berfungsi menjaga agar gen-gen profag tidak aktif.</p>
-      <p>d. Pembelahan </p>
-      <p>Jika sel inang membelah, setiap anakannya akan mewarisi profag. Profag dapat diinduksi menjadi aktif, sehingga mengakibatkan terjadinya daur litik. </p>
-      <p>e. Sintesis </p>
-      <p>Profag aktif dan keluar dari kromosom bakteri, sehingga DNA bakteri (sel inang) hancur. Kemudian, terjadi fase replikasi DNA bakteriofag, sintesis bagian-bagian tubuh virus, dan seterusnya seperti pada daur litik.</p>
-      <p></p>
-    </div>
+      <div className="card-container">
+        <img src="./images/Virus3.jpg" alt="Virus" className="card-img" />
+        <h1 className="card-title">Sejarah Virus</h1>
+        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
+        <Link to="/virus-details" className="card-btn">Learn More</Link>
+      </div>
+
+      <div className="card-container">
+        <img src="./images/Virus4.jpeg" alt="Virus" className="card-img" />
+        <h1 className="card-title">Sejarah Virus</h1>
+        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
+        <Link to="/virus-details" className="card-btn">Learn More</Link>
+      </div>
+
+      <div className="card-container">
+        <img src="./images/Virus5.jpg" alt="Virus" className="card-img" />
+        <h1 className="card-title">Sejarah Virus</h1>
+        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
+        <Link to="/virus-details" className="card-btn">Learn More</Link>
+      </div>
+
     </div>
   );
-};
+}
 
 export default Virus;
