9276ebae57c16448f4465492b3250d30b40403df
Chantique Nababan <chantiquenababan01@gmail.com>
2024-11-29 10:58:37 +0700

    Menambahkan Struktur Virus (virus.js dan css)

--- src/pages/Virus.js
+++ src/pages/Virus.js
@@ -31,41 +31,32 @@ function Virus() {
         <p><strong>d. Wendell Meredith Stanley.</strong> Pada tahun 1935, Wendell Meredith Stanley, seorang ilmuwan Amerika berhasil mengkristalkan partikel penyebab penyakit pada tanaman tembakau. Penyakit ini kemudian dikenal dengan nama Tobacco Mosaic Virus (TMV).</p>
       </div>  
 
-      <h2>Yuk ketahui lebih lanjut !</h2>
-
       <div className="card-container">
         <img src="./images/Virus1.jpeg" alt="Virus" className="card-img" />
         <h1 className="card-title">Sejarah Virus</h1>
-        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
-        <Link to="/virus-details" className="card-btn">Learn More</Link>
+        <p className="card-description">Ketahui Struktur dari pengetahuan tentang virus.</p>
+        <Link to="/StrukturV" className="card-btn">Learn More</Link>
       </div>
 
       <div className="card-container">
         <img src="./images/Virus2.jpg" alt="Virus" className="card-img" />
-        <h1 className="card-title">Sejarah Virus</h1>
-        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
-        <Link to="/virus-details" className="card-btn">Learn More</Link>
+        <h1 className="card-title">Bentuk Virus</h1>
+        <p className="card-description">Ketahui bentuk dari pengetahuan tentang virus.</p>
+        <Link to="/BentukV" className="card-btn">Learn More</Link>
       </div>
 
       <div className="card-container">
         <img src="./images/Virus3.jpg" alt="Virus" className="card-img" />
-        <h1 className="card-title">Sejarah Virus</h1>
-        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
-        <Link to="/virus-details" className="card-btn">Learn More</Link>
+        <h1 className="card-title">Replikasi Virus</h1>
+        <p className="card-description">Ketahui Replikasi dari pengetahuan tentang virus.</p>
+        <Link to="/ReplikasiV" className="card-btn">Learn More</Link>
       </div>
 
       <div className="card-container">
         <img src="./images/Virus4.jpeg" alt="Virus" className="card-img" />
-        <h1 className="card-title">Sejarah Virus</h1>
-        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
-        <Link to="/virus-details" className="card-btn">Learn More</Link>
-      </div>
-
-      <div className="card-container">
-        <img src="./images/Virus5.jpg" alt="Virus" className="card-img" />
-        <h1 className="card-title">Sejarah Virus</h1>
-        <p className="card-description">Temukan asal-usul dan perkembangan pengetahuan tentang virus.</p>
-        <Link to="/virus-details" className="card-btn">Learn More</Link>
+        <h1 className="card-title">Penyakit & Pencegahan Virus</h1>
+        <p className="card-description">Ketahui Penyakit serta Pencegahan dari Virus.</p>
+        <Link to="/PenyakitV" className="card-btn">Learn More</Link>
       </div>
 
     </div>
---
+++ src/pages/klasifikasi/StrukturV.js
@@ -0,0 +1,51 @@
+import React from "react";
+import '../../styles/strukturv.css';
+
+function StrukturV() {
+    return (
+        <div className="struktur-container">
+            <h5>
+                Virus adalah mikroorganisme yang sangat kecil dan tidak dapat dilihat dengan mata telanjang. 
+                Mereka dapat menyebabkan berbagai penyakit pada manusia, hewan, dan tumbuhan. 
+                Meskipun mereka tidak hidup seperti sel, virus dapat memperbanyak diri dengan menginfeksi sel inang.
+            </h5><br></br>
+            
+            <h1>Ciri-ciri Virus</h1>
+            <p>a.Virus berukuran sangat kecil, berkisar 0,02-0,3 µm (1 µm = 1/1.000 mm), dan paling besar berukuran 200 µm, karena itu virus hanya dapat dilihat dengan mikroskop elektron. </p>
+            <p>b.Tubuh virus terdiri atas selubung proton (kapsid), dan bahan inti. Bahan inti berupa RNA (Ribonucleic acid) dan DNA (Deoxiribonucleic acid). </p>
+            <p>c.Virus tidak mempunyai membran dan organel-organel sel yang penting bagi kehidupan. </p>
+            <p>d.Virus hanya dapat bereproduksi jika berada dalam sel hidup atau jaringan hidup. </p>
+            <p>e.Biasanya stabil pada pH 5.0 sampai 9.0. </p>
+            <p>f.Virus dapat dikristalkan seperti benda mati. Bentuk virus bermacam-macam ada yang berbentuk batang, bola, atau bulat, berbetuk peluru, dan berbentuk</p>
+            <p>g.Aktivitas virus dapat dihilangkan oleh sinar ultra ungu dan sinar X tetapi zat antibiotik dan zat antibakteri lain tidak berpengaruh terhadapnya.</p> <br></br>
+
+            
+            <h1>Struktur Virus</h1>
+            <p>Virus tidak termasuk sel (aseluler), karena tidak memiliki bagian-bagian sel seperti dinding sel, membran sel, sitoplasma, inti sel, dan organel-organel lainnya. Partikel virus yang lengkap disebut virion. Secara umum, struktur virus diwakili oleh bakteriofag yang berbentuk seperti huruf T.</p>
+        <div className="image-container">
+            <img className="image"
+            src="https://awsimages.detik.net.id/community/media/visual/2022/11/22/modul-pembelajaran-biologi-kemendikbud.jpeg?w=600&q=90"/>
+        </div>
+            <ul>
+                <li>a. Kepala </li>
+                <p>Kepala merupakan bagian dalam kepala virus berisi asam nukleat, sedangkan bagian luarnya diselubungi oleh kapsid. Kepala virus bakteriofag berbentuk polihedral dengan jenis asam nukleat DNA</p>
+                <li>b. Leher</li>
+                <p>Leher merupakan bagian yang menghubungkan kepala dan ekor. Leher juga menjadi saluran keluarnya asam nukleat menuju ekor.</p>
+                <li>c. Ekor </li>
+                <p>Ekor merupakan bagian yang berfungsi untuk menempel pada sel inang. Ekor terdiri atas serabut ekor dan lempeng dasar. Serabut ekor berfungsi melekatkan diri pada sel inang. Sementara itu, lempeng dasar yang berisi jarum penusuk berfungsi untuk menginjeksikan DNA ke dalam sel inang.</p>
+            </ul><br></br>
+            <h1>Fakta Menarik Tentang Virus </h1>
+            <ul>
+                <li>Virus dapat berukuran antara 20 hingga 300 nanometer.</li>
+                <li>Beberapa virus dapat menyebabkan penyakit serius, seperti HIV dan influenza.</li>
+                <li>Virus dapat bermutasi dengan cepat, membuatnya sulit untuk diobati.</li>
+                <li>Tidak semua virus berbahaya; beberapa virus bahkan dapat digunakan dalam terapi gen.</li>
+            </ul>
+            <footer>
+                <p>Untuk informasi lebih lanjut, kunjungi situs web kesehatan terkemuka.</p>
+            </footer>
+        </div>
+    );
+}
+
+export default StrukturV;
